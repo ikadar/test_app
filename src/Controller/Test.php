d6eb2d8590956d3be2c1e6b41f890c0a02d1f86a
Istvan Kadar <odesk.kadari@gmail.com>
2022-05-26 12:26:59 +0200

    Create Test.php

---
+++ src/Controller/Test.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Test\App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Test extends AbstractController
+{
+    /**
+     * @Route("/test1", methods={"GET"})
+     */
+    public function test1() : JsonResponse
+    {
+        $info = "TEST01";
+
+        dump($info);
+        die();
+
+        return new JsonResponse(
+            $info,
+            JsonResponse::HTTP_OK
+        );
+    }
+
+    /**
+     * @Route("/test2", methods={"GET"})
+     */
+    public function test2() : JsonResponse
+    {
+        $info = "TEST02";
+
+        dump($info);
+        die();
+
+        return new JsonResponse(
+            $info,
+            JsonResponse::HTTP_OK
+        );
+    }
+
+}
